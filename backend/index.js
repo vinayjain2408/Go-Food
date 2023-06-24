53367d1ee3d0b78a9119180d77d0b87e8d81576b
vinayjain2408 <vinay98292jain@gmail.com>
2023-06-24 13:17:19 +0530

    link node to react

--- backend/index.js
+++ backend/index.js
@@ -1,85 +1,169 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongodb = require('./db')
-mongodb()
+// const express = require('express')
+// const app = express()
+// const port = 5000
+// const mongodb = require('./db')
+// mongodb()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.use(express.json())
 
-app.use("/api" ,require("./Routes/CreateUser"))
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-control-Allow-Origin" , "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With , Centent-Type ,Accept"
+//   );
+//   next()
+// }
+// )
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.use(express.json())
+
+// app.use("/api" ,require("./Routes/CreateUser"))
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+// // const express = require('express');
+// // const app = express();
+// // const port = 5000;
+// // const mongodb = require('./db');
+
+// // mongodb().then(() => {
+// //   app.listen(port, () => {
+// //     console.log(`Example app listening on port ${port}`);
+// //   });
+// // }).catch((error) => {
+// //   console.error('Error connecting to the database:', error);
+// // });
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World!');
+// // });
+
+// // app.use(express.json());
+
+// // app.use('/api', require('./Routes/CreateUser'));
+
+
+
+
+// // import express from "express";
+// // import cors from "cors";
+// // import connection from "./db/index.js";
+// // import AdminRouter from './routes/admin.js'
+// // import UserRouter from './routes/user.js'
+
+// // import mongoose from "mongoose"
+
+// // const connection = mongoose.connect(`mongodb+srv://vinayjain:vinay1234@cluster0.yev5lmn.mongodb.net/?retryWrites=true&w=majority`, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: true,
+// // })
+
+// // const app = express();
+
+// // //ALLOW INCOMING REQUESTS FROM THIS ORIGIN AS WELL
+// // app.use(cors({ origin: "http://localhost:5174" }));
+// // app.use(express.urlencoded())
+// // app.use(express.json());
+
+// // //SSR: Server Side Rendered with Pug
+// // app.set("view engine", "pug");
+
+
+// // app.use("/admin", AdminRouter)
+// // app.use("/user", UserRouter)
+
+
+
+// // const port = process.env.PORT || 8001;
+
+// // connection
+// //   .then(() =>
+// //     app.listen(port, () => {
+// //       console.log(`Server listening on port ${port}`);
+// //     })
+// //   )
+// //   .catch((err) => {
+// //     console.log("Error connecting to MongoDB Atlas", err);
+// //   });
+
+
 
 
 
 
 // const express = require('express');
 // const app = express();
+// const cors = require("cors")
 // const port = 5000;
 // const mongodb = require('./db');
 
-// mongodb().then(() => {
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`);
-//   });
-// }).catch((error) => {
-//   console.error('Error connecting to the database:', error);
-// });
+// mongodb();
+// app.use(cors({origin:"http://localhost:3000"}))
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
 // });
 
 // app.use(express.json());
 
 // app.use('/api', require('./Routes/CreateUser'));
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
 
 
 
-// import express from "express";
-// import cors from "cors";
-// import connection from "./db/index.js";
-// import AdminRouter from './routes/admin.js'
-// import UserRouter from './routes/user.js'
-
-// import mongoose from "mongoose"
-
-// const connection = mongoose.connect(`mongodb+srv://vinayjain:vinay1234@cluster0.yev5lmn.mongodb.net/?retryWrites=true&w=majority`, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-// })
-
-// const app = express();
-
-// //ALLOW INCOMING REQUESTS FROM THIS ORIGIN AS WELL
-// app.use(cors({ origin: "http://localhost:5174" }));
-// app.use(express.urlencoded())
-// app.use(express.json());
-
-// //SSR: Server Side Rendered with Pug
-// app.set("view engine", "pug");
-
-
-// app.use("/admin", AdminRouter)
-// app.use("/user", UserRouter)
 
 
 
-// const port = process.env.PORT || 8001;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+const mongodb = require('./db');
 
-// connection
-//   .then(() =>
-//     app.listen(port, () => {
-//       console.log(`Server listening on port ${port}`);
-//     })
-//   )
-//   .catch((err) => {
-//     console.log("Error connecting to MongoDB Atlas", err);
-//   });
+mongodb();
+
+app.use(cors({ origin: 'https://localhost:3000' }));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use('/api', require('./Routes/CreateUser'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- frontend/src/Screen/Signup.js
+++ frontend/src/Screen/Signup.js
@@ -1,68 +1,135 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from "axios"
+
+// function Signup() {
+//   const [credentials, setCredentials] = useState({
+//     name: "",
+//     email: "",
+//     password: "",
+//     geolocation: ""
+//   });
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const response = await axios("https://localhost:5000/api/createuser", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json" // Corrected header key
+  //     },
+  //     body: JSON.stringify({
+  //       name: credentials.name,
+  //       email: credentials.email,
+  //       password: credentials.password,
+  //       geolocation: credentials.geolocation // Corrected property name
+  //     })
+  //   });
+  //   const json = await response.json();
+  //   console.log(json);
+
+  //   if (!json.success) {
+  //     alert("Enter Valid Data");
+  //   }
+  // };
+
+//   const onChange = (e) => {
+//     setCredentials({ ...credentials, [e.target.name]: e.target.value });
+//   };
+
+//   return (
+//     <div className='container'>
+//       <form onSubmit={handleSubmit}>
+//         <div className="mb-3">
+//           <label htmlFor="name">User Name</label>
+//           <input type="text" className="form-control" placeholder="Enter Name" name="name" value={credentials.name} onChange={onChange} />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="email">Email address</label>
+//           <input type="email" className="form-control" name="email" value={credentials.email} id="email" aria-describedby="emailHelp" placeholder="Enter email" onChange={onChange} />
+//           <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="password">Password</label>
+//           <input type="password" className="form-control" name="password" value={credentials.password} id="password" placeholder="Password" onChange={onChange} />
+//         </div>
+//         <div className="mb-3">
+//           <label htmlFor="geolocation">Address</label>
+//           <input type="text" className="form-control" name="geolocation" value={credentials.geolocation} placeholder="Enter Address" onChange={onChange} />
+//         </div>
+//         <button type="submit" className="btn btn-success">Submit</button>
+//         <Link to="/login" className='m-3 btn btn-danger'>Already a User</Link>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Signup;
+
+
+
+
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from "axios";
 
 function Signup() {
-    const [credentials , setCredential] = useState({name:"",email:"",password:"",geolocation:""})
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    geolocation: ""
+  });
 
-    const handlesubmit = async(e)=>{
-        e.preventDefault();
-        const response = fetch("https://localhost:5000/api/createuser" ,{
-            method:"POST",
-            header:{
-                "content=type":"application/json"
-            },
-            body:JSON.stringify({
-                name:credentials.name,
-                email:credentials.email,
-                password:credentials.password,
-                location:credentials.geolocation
-            })
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://localhost:5000/api/createuser", {
+        name: credentials.name,
+        email: credentials.email,
+        password: credentials.password,
+        geolocation: credentials.geolocation
+      });
+      console.log(response.data);
+
+      if (!response.data.success) {
+        alert("Enter Valid Data");
+      }
     }
-
-
-    const onchange = (e)=>{
-        setCredential({...credentials , [e.target.name]:e.target.value})
+     catch (error) {
+      console.log(error);
     }
+  };
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
-    <>
     <div className='container'>
-
-  
-    <form onSubmit={handlesubmit}>
-    <div className="mb-3">
-    <label for="User Name">User Name</label>
-    <input type="text" className="form-control" placeholder="Enter Name" name="name" value={credentials.name} onChange={onchange}/>
-    
-  </div>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" name="email" value={credentials.email} id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" name="password" value={credentials.password} id="exampleInputPassword1"  placeholder="Password" />
-  </div>
-  <div className="mb-3">
-    <label for="Address">Address</label>
-    <input type="text" className="form-control" name="geolocation" value={credentials.geolocation} placeholder="Enter Address" />
-    
-  </div>
-  {/* <div className="mb-3">
-    <label for="location">Location</label>
-    <input type="text" className="form-control" name="name" value={credentials.name} placeholder="Enter Name" />
-    
-  </div> */}
-  
-  <button type="submit" className="btn btn-success">Submit</button>
-  <Link to="/login" className='m-3 btn btn-danger'>Already a User</Link>
-</form>
-
-</div>
-    </>
-
-  )
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name">User Name</label>
+          <input type="text" className="form-control" placeholder="Enter Name" name="name" value={credentials.name} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" name="email" value={credentials.email} id="email" aria-describedby="emailHelp" placeholder="Enter email" onChange={onChange} />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" className="form-control" name="password" value={credentials.password} id="password" placeholder="Password" onChange={onChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="geolocation">Address</label>
+          <input type="text" className="form-control" name="geolocation" value={credentials.geolocation} placeholder="Enter Address" onChange={onChange} />
+        </div>
+        <button type="submit" className="btn btn-success">Submit</button>
+        <Link to="/login" className='m-3 btn btn-danger'>Already a User</Link>
+      </form>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
