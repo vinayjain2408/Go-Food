e580622630a2a0e61e935c5642f805a7867614e1
vinayjain2408 <vinay98292jain@gmail.com>
2023-06-25 13:32:35 +0530

    login page update

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -57,6 +57,42 @@
 // module.exports = routes;
 
 
+// again try
+
+// const express = require("express");
+// const routes = express.Router();
+// const { body, validationResult } = require('express-validator');
+// const User = require("../models/User");
+
+// routes.post("/createuser", [
+//   body('email').isEmail().withMessage('Invalid email format'),
+//   body("name").isLength({ min: 5 }).withMessage('Name must be at least 5 characters long'),
+//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+// ], async (req, res) => {
+//   try {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+  
+//     const { name, email, password, location } = req.body;
+//     const user = await User.create({ name, email, password, location });
+//     console.log("User created successfully");
+//     res.json({ success: true, user });
+//   }
+  
+//   catch (error) {
+//     console.error(error);
+//     res.status(500).json({ success: false, error: 'Failed to create user' });
+//   }
+// });
+
+// module.exports = routes;
+
+
+
+
+
 
 
 
@@ -82,47 +118,46 @@ routes.post("/createuser", [
     res.json({ success: true, user });
   } catch (error) {
     console.error(error);
+    res.status(600).json({ success: false, error: 'Failed to create user' });
+  }
+});
+ 
+
+
+routes.post("/loginuser",
+[
+  body('email').isEmail().withMessage('Invalid email format'),
+  body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+] ,async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  let email = req.body.email;
+  try {
+    
+    let userData = await User.findOne({email});
+
+    if(!userData){
+      return res.status(400).json({errors:"try logging with correct credentials"})
+    }
+
+    if(req.body.password !== userData.password){
+      return res.status(600).json({errors:"try logging with correct credentials"})
+    }
+
+    return res.json({success:true})
+
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ success: false, error: 'Failed to create user' });
   }
 });
 
 
-
-
-// routes.post("/loginuser",
-// [
-//   body('email').isEmail().withMessage('Invalid email format'),
-//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
-// ] ,async (req, res) => {
-
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-
-//   let email = req.body.email;
-//   try {
-    
-//     let userData = await User.findOne({email});
-
-//     if(!userData){
-//       return res.status(400).json({errors:"try logging with correct credentials"})
-//     }
-
-//     if(req.body.password !== userData.password){
-//       return res.status(400).json({errors:"try logging with correct credentials"})
-//     }
-
-//     return res.json({success:true})
-
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, error: 'Failed to create user' });
-//   }
-// });
 module.exports = routes;
-
-
 
 
 
