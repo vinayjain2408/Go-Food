58f800b84ba6cd56cd1126bceb137122a2c647b4
vinayjain2408 <vinay98292jain@gmail.com>
2023-06-25 14:36:58 +0530

    issue in login

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -96,9 +96,76 @@
 
 
 
+// const express = require("express");
+// const routes = express.Router();
+// const { body, validationResult } = require('express-validator');
+// const User = require("../models/User");
+
+// routes.post("/createuser", [
+//   body('email').isEmail().withMessage('Invalid email format'),
+//   body("name").isLength({ min: 5 }).withMessage('Name must be at least 5 characters long'),
+//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+// ], async (req, res) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ errors: errors.array() });
+//   }
+  
+//   try {
+//     const { name, email, password, location } = req.body;
+//     const user = await User.create({ name, email, password, location });
+//     console.log("User created successfully");
+//     res.json({ success: true, user });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(600).json({ success: false, error: 'Failed to create user' });
+//   }
+// });
+ 
+
+
+// routes.post("/loginuser",
+// [
+//   body('email').isEmail().withMessage('Invalid email format'),
+//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+// ] ,async (req, res) => {
+
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ errors: errors.array() });
+//   }
+
+//   let email = req.body.email;
+//   try {
+    
+//     let userData = await User.findOne({email});
+
+//     if(!userData){
+//       return res.status(400).json({errors:"try logging with correct credentials"})
+//     }
+
+//     if(req.body.password !== userData.password){
+//       return res.status(600).json({errors:"try logging with correct credentials"})
+//     }
+
+//     return res.json({success:true})
+
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ success: false, error: 'Failed to create user' });
+//   }
+// });
+
+
+// module.exports = routes;
+
+
+
+
 const express = require("express");
 const routes = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 const User = require("../models/User");
 
 routes.post("/createuser", [
@@ -113,51 +180,50 @@ routes.post("/createuser", [
   
   try {
     const { name, email, password, location } = req.body;
-    const user = await User.create({ name, email, password, location });
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const user = await User.create({ name, email, password: hashedPassword, location });
     console.log("User created successfully");
     res.json({ success: true, user });
-  } catch (error) {
-    console.error(error);
-    res.status(600).json({ success: false, error: 'Failed to create user' });
-  }
-});
- 
-
-
-routes.post("/loginuser",
-[
-  body('email').isEmail().withMessage('Invalid email format'),
-  body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
-] ,async (req, res) => {
-
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  let email = req.body.email;
-  try {
-    
-    let userData = await User.findOne({email});
-
-    if(!userData){
-      return res.status(400).json({errors:"try logging with correct credentials"})
-    }
-
-    if(req.body.password !== userData.password){
-      return res.status(600).json({errors:"try logging with correct credentials"})
-    }
-
-    return res.json({success:true})
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Failed to create user' });
   }
 });
 
+routes.post("/loginuser", [
+  body('email').isEmail().withMessage('Invalid email format'),
+  body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ errors: "Invalid credentials" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ errors: "Invalid credentials" });
+    }
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, error: 'Failed to log in' });
+  }
+});
 
 module.exports = routes;
+
+
+
 
 
 
