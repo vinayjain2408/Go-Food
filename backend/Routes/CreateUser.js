c0ff129420532c486daec77510be1642bf02a34f
vinayjain2408 <vinay98292jain@gmail.com>
2023-06-26 22:49:31 +0530

    Password Generater

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,105 +1,9 @@
-// const express = require("express");
-// const routes = express.Router();
-// const { body, validationResult } = require('express-validator');
-// const User = require("../models/User");
-
-// routes.post("/createuser", [
-//   body('email').isEmail().withMessage('Invalid email format'),
-//   body("name").isLength({ min: 5 }).withMessage('Name must be at least 5 characters long'),
-//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
-// ], async (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-  
-//   try {
-//     const { name, email, password, geolocation } = req.body;
-//     const user = await User.create({ name, email, password, geolocation });
-//     console.log("User created successfully");
-//     res.json({ success: true, user });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, error: 'Failed to create user' });
-//   }
-// });
-
-
-
-// routes.post("/loginuser", [
-//   body('email').isEmail().withMessage('Invalid email format'),
-//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
-// ], async (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-
-//   try {
-//     const { email, password } = req.body;
-//     const userData = await User.findOne({ email });
-
-//     if (!userData) {
-//       return res.status(400).json({ error: "Invalid email or password" });
-//     }
-
-//     if (password !== userData.password) {
-//       return res.status(400).json({ error: "Invalid email or password" });
-//     }
-
-//     return res.json({ success: true });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, error: 'Failed to login' });
-//   }
-// });
-
-// module.exports = routes;
-
-
-// again try
-
-// const express = require("express");
-// const routes = express.Router();
-// const { body, validationResult } = require('express-validator');
-// const User = require("../models/User");
-
-// routes.post("/createuser", [
-//   body('email').isEmail().withMessage('Invalid email format'),
-//   body("name").isLength({ min: 5 }).withMessage('Name must be at least 5 characters long'),
-//   body("password").isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
-// ], async (req, res) => {
-//   try {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-  
-//     const { name, email, password, location } = req.body;
-//     const user = await User.create({ name, email, password, location });
-//     console.log("User created successfully");
-//     res.json({ success: true, user });
-//   }
-  
-//   catch (error) {
-//     console.error(error);
-//     res.status(500).json({ success: false, error: 'Failed to create user' });
-//   }
-// });
-
-// module.exports = routes;
-
-
-
-
-
-
-
-
 const express = require("express");
 const routes = express.Router();
 const { body, validationResult } = require('express-validator');
 const User = require("../models/User");
+
+const bcrypt = require("bcrypt")
 
 routes.post("/createuser", [
   body('email').isEmail().withMessage('Invalid email format'),
@@ -111,12 +15,27 @@ routes.post("/createuser", [
     return res.status(400).json({ errors: errors.array() });
   }
   
-  try {
-    const { name, email, password, location } = req.body;
-    const user = await User.create({ name, email, password, location });
-    console.log("User created successfully");
-    res.json({ success: true, user });
-  } catch (error) {
+  
+  // try {
+  //   const { name, email, password, location } = req.body;
+  //   const user = await User.create({ name, email, password, location });
+  //   console.log("User created successfully");
+  //   res.json({ success: true, user });
+  // } 
+
+
+  const salt = await bcrypt.genSalt(10);
+  let securePassword = await bcrypt.hash(req.body.password , salt)
+  try{
+    await User.create({
+      name :req.body.name,
+      password:securePassword,
+      email:req.body.email,
+      location:req.body.location
+    }).then(res.json({success:true}))
+  }
+  
+  catch (error) {
     console.error(error);
     res.status(600).json({ success: false, error: 'Failed to create user' });
   }
